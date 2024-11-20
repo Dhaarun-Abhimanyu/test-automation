572938bc7afe619eb4584f3d555ec37adc13ce02
Dhaarun <dhaarunabhimanyu@gmail.com>
2024-11-20 21:04:38 +0530

    added some basic routes

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
+app.use(express.json())
+
 app.get('/', (req,res) => {
     res.send({msg: "Skill issue"})
+})
+
+app.get('/route2', (req,res) => {
+    res.send({msg: "Route 2"})
+})
+
+app.post('/route3', (req,res) => {
+    const {name} = req.body
+    res.send({msg: `Hello ${name}`})
 })
 
 app.listen(process.env.PORT, () => {
